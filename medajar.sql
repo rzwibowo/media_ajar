bc9a065d805e420f56a5945a79dd18f84ccb8f2f
Rizki A. Wibowo <d7revenge@gmail.com>
2017-03-05 21:29:16 +0700

    update db

--- medajar.sql
+++ medajar.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
+-- version 4.4.12
+-- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: 25 Feb 2017 pada 08.17
--- Versi Server: 10.1.21-MariaDB
--- PHP Version: 7.1.1
+-- Host: 127.0.0.1
+-- Generation Time: Mar 05, 2017 at 03:28 PM
+-- Server version: 5.6.25
+-- PHP Version: 5.6.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,14 +19,16 @@ SET time_zone = "+00:00";
 --
 -- Database: `medajar`
 --
+CREATE DATABASE IF NOT EXISTS `medajar` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `medajar`;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `provinsi`
+-- Table structure for table `provinsi`
 --
 
-CREATE TABLE `provinsi` (
+CREATE TABLE IF NOT EXISTS `provinsi` (
   `id_prov` int(11) NOT NULL,
   `nama_prov` varchar(30) NOT NULL,
   `ibukota` varchar(30) NOT NULL,
@@ -38,12 +40,24 @@ CREATE TABLE `provinsi` (
   `suku` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data untuk tabel `provinsi`
+-- Table structure for table `user`
 --
 
-INSERT INTO `provinsi` (`id_prov`, `nama_prov`, `ibukota`, `jml_penduduk`, `luas_wilayah`, `rumah_adat`, `tari_adat`, `bhs_daerah`, `suku`) VALUES
-(1, 'Jogja', 'Jogja', 10999, 7, 'Kraton', 'Sinden', 'Jawa', 'Jawa');
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`) VALUES
+(1, 'ngadimin', '5449ccea16d1cc73990727cd835e45');
 
 --
 -- Indexes for dumped tables
@@ -56,6 +70,12 @@ ALTER TABLE `provinsi`
   ADD PRIMARY KEY (`id_prov`);
 
 --
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -63,7 +83,12 @@ ALTER TABLE `provinsi`
 -- AUTO_INCREMENT for table `provinsi`
 --
 ALTER TABLE `provinsi`
-  MODIFY `id_prov` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_prov` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
